80d242ec3e3bf0844a15eb78dfdff68a5589745b
pippozq <zengqi0529@163.com>
2022-11-20 15:43:28 +0800

    update jwt

--- example/example.go
+++ example/example.go
@@ -1,83 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "github.com/kunlun-qilian/jwt"
-    "time"
+	"fmt"
+	"github.com/kunlun-qilian/jwt"
+	"time"
 )
 
 func main() {
-    jwtProvider := jwt.JWKSProvider{
-        PrivateKey: `LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFb3dJQkFBS0NBUUVBbGpoN3ZQWWhzR1Ywa3NVMXRhV2VLSWxIc1phSTYvbjdDNStYTzJ5TVBEenpQaVdCCllWKy94UFdwQ253eVdVdDVoWk5samJxVTV5MlR3SHM3OXRJaHpyay9zalBSck5PSjE3cVpoK3pTcUF4NFZCK3AKc3ZYR2lYVytOZTRBc3UwU09zamhVZGJhR3NEVWppY21UTVlyeHlpNytZZm16UkFJTDluY0wrREZ0UWpEUVh0ZwpObUhHMVU2MUNlbW5tdHhDdzZYQkdMSHUwd01oblJ5dlFlSlhOOW1tQ0JZYnNjSTNUQ0lXeXRoYWtVenJIdFVyCm5ESlJKZG5IUngzY2ptMi9LUHdUT2xsSW1saDNRRFJOQXM4OTIzenF6R1pVTG8wb3VCcUthOUFjOWxtTXExTWMKQitkeXdCaFhnUGtHL1hmdkpqNENvYVRTUDVGTTYwcHVYdWxVK1FJREFRQUJBb0lCQUZwSDZmM2Z3dEY3dC9xNQpjUlVSU3lNN2xnM2gxYnBVaTQ4cHc3OGY5b3dCYVlMUkVaZk83cmJWbVZsRzJRK1hiNXBhd203U1VzazVPQkkzClBndVJzR1hJS2NMVXA5QUJkbGRqdXYvWTBhWFRrTUdtSWR2L0grbmZESWptMkI2d29nTVlWV3BWQU5HUlVHMTYKaytjU05wOUVHT3pYdzFzOHBsN3p4UVExUnowTDhtSnAyTEwxSEJ5UnlmbDRJVDIvYzIwNTFlWWFKY3hUamFubgpIMlpjNWxFT3dtN0NMcWk2NWNRVE1IZW5zMzdTOHBPOFVxVXU5SDN2NzZpcGs0L21ZRUJMdC8rRUs3N0ExbU1hCjBXQ0ZXaDNXOG5HVExyM3M1aGNDWVVwOEY2Z1k1MDkyeGFvV3J5Nncwdnh5YkdpNGM2QjZzNVczOWdkNGtHTmYKb1BURUlURUNnWUVBeUFDclQraGxsNjVqZWpaR2Y4eTJJajVONGE4MVg2Q3JjUlFMZXBwRWpHM3lVRHhFZHhyMwp5REw5am1HUFUxOXpQeWNsaWhJMzV0UHdsbFUvRGRpSHNucHNJR3RRWmJMa0tER25TbzV0UWtJaC9vRzM5U3NKCjNZcmhPVkVFeUVnY3ZNK0N1Q2ZBb0xmcW5jNWl6WFZ1WlQ4RU5sUi9Sby94OS85dlk4NUZWSFVDZ1lFQXdFZW4Kdy9Fb3NXdFNSV0ZoUy83eGgvVXNmSzFMOFdWb1hIUWJmSENUWlJWSGNyajQrYy83MGo1T3lJU1ZIK0IzUUltZgpVOWlNUzJvL3lCVXdDMm5zMVlxV1VQRGxoVEY3SnVpOXhEbWx4cXZubGUyVlROTytPMlBEK2p0WGdCakUya0Z3CiswanFvZCs1bXdMZUJ0Mno5bTEzYlVtUmZpVUFYWUg0YmlzOVhmVUNnWUVBbGJtYWhobXVaRjBTNzV6T0xrSnMKWHpwUlI1REkzaXdEN0lWYkNvK09uYXA1YW9PVHBhNjByRlV2NkhVMHZPK0o4VTgzRlNRS1lXMXNnTDZVazZMMApBek1PMno0N1U4Y1djdGlwS25GeGJkYmdhQTFvVDh2R2VPbk5MZ0Z2R1JpVEd1NG1LQUxxZStielp1dm9uM000CkQrZWJHYWtzRndFUDNkNkYzeXUrVHprQ2dZQTQ1RHE0V0IrQk4wNzFabFhDaGFGelo5Q001ejJrSkN3WHh1Ym4KRCt3Y3FZb2xZS09TVHI2a3UzaldEWnlOL1AxcjVBeDNZNGhIUEIyNUZzUExiTUQ5Z2U3dDdna0xPdFBFZEhMSgpuNEQwWXFLNEVyN3RKMjJPNXZyeWpDSmNyWGQ1V0ltVVlFUEVONDBVVjJuWVFEcmlQQXR0RTZwdjE0VGZKZmlhCnFFS1ZEUUtCZ0RuNktnSEU2NmY2VmR3UjFYN3R2WkVSOUo5d1pUWmJCZHpLZm81OWs4M0lRUXRFY0dxbnM2N0kKaXRuM1NZR3htUk5La2VzazNBQ2pKbTNEZyszTjNxakYrZjR1MTMyMmNTc3JCc2dQUEZIMGtwcGlyQS9tNmsvagowdmxBU1ZBa05WbHpZM3RONXJxRWtER2RFL21PNnE2ZlhMUkZkY201RGh2TzcwcHdhOUNhCi0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0tCg==`,
-        PublicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAljh7vPYhsGV0ksU1taWe
-KIlHsZaI6/n7C5+XO2yMPDzzPiWBYV+/xPWpCnwyWUt5hZNljbqU5y2TwHs79tIh
-zrk/sjPRrNOJ17qZh+zSqAx4VB+psvXGiXW+Ne4Asu0SOsjhUdbaGsDUjicmTMYr
-xyi7+YfmzRAIL9ncL+DFtQjDQXtgNmHG1U61CemnmtxCw6XBGLHu0wMhnRyvQeJX
-N9mmCBYbscI3TCIWythakUzrHtUrnDJRJdnHRx3cjm2/KPwTOllImlh3QDRNAs89
-23zqzGZULo0ouBqKa9Ac9lmMq1McB+dywBhXgPkG/XfvJj4CoaTSP5FM60puXulU
-+QIDAQAB
------END PUBLIC KEY-----`,
-    }
+	jwtMgr := jwt.NewJwtMgr(`LS0tLS1CRUdJTiBSU0EgUFJJVkFURSBLRVktLS0tLQpNSUlFb3dJQkFBS0NBUUVBbGpoN3ZQWWhzR1Ywa3NVMXRhV2VLSWxIc1phSTYvbjdDNStYTzJ5TVBEenpQaVdCCllWKy94UFdwQ253eVdVdDVoWk5samJxVTV5MlR3SHM3OXRJaHpyay9zalBSck5PSjE3cVpoK3pTcUF4NFZCK3AKc3ZYR2lYVytOZTRBc3UwU09zamhVZGJhR3NEVWppY21UTVlyeHlpNytZZm16UkFJTDluY0wrREZ0UWpEUVh0ZwpObUhHMVU2MUNlbW5tdHhDdzZYQkdMSHUwd01oblJ5dlFlSlhOOW1tQ0JZYnNjSTNUQ0lXeXRoYWtVenJIdFVyCm5ESlJKZG5IUngzY2ptMi9LUHdUT2xsSW1saDNRRFJOQXM4OTIzenF6R1pVTG8wb3VCcUthOUFjOWxtTXExTWMKQitkeXdCaFhnUGtHL1hmdkpqNENvYVRTUDVGTTYwcHVYdWxVK1FJREFRQUJBb0lCQUZwSDZmM2Z3dEY3dC9xNQpjUlVSU3lNN2xnM2gxYnBVaTQ4cHc3OGY5b3dCYVlMUkVaZk83cmJWbVZsRzJRK1hiNXBhd203U1VzazVPQkkzClBndVJzR1hJS2NMVXA5QUJkbGRqdXYvWTBhWFRrTUdtSWR2L0grbmZESWptMkI2d29nTVlWV3BWQU5HUlVHMTYKaytjU05wOUVHT3pYdzFzOHBsN3p4UVExUnowTDhtSnAyTEwxSEJ5UnlmbDRJVDIvYzIwNTFlWWFKY3hUamFubgpIMlpjNWxFT3dtN0NMcWk2NWNRVE1IZW5zMzdTOHBPOFVxVXU5SDN2NzZpcGs0L21ZRUJMdC8rRUs3N0ExbU1hCjBXQ0ZXaDNXOG5HVExyM3M1aGNDWVVwOEY2Z1k1MDkyeGFvV3J5Nncwdnh5YkdpNGM2QjZzNVczOWdkNGtHTmYKb1BURUlURUNnWUVBeUFDclQraGxsNjVqZWpaR2Y4eTJJajVONGE4MVg2Q3JjUlFMZXBwRWpHM3lVRHhFZHhyMwp5REw5am1HUFUxOXpQeWNsaWhJMzV0UHdsbFUvRGRpSHNucHNJR3RRWmJMa0tER25TbzV0UWtJaC9vRzM5U3NKCjNZcmhPVkVFeUVnY3ZNK0N1Q2ZBb0xmcW5jNWl6WFZ1WlQ4RU5sUi9Sby94OS85dlk4NUZWSFVDZ1lFQXdFZW4Kdy9Fb3NXdFNSV0ZoUy83eGgvVXNmSzFMOFdWb1hIUWJmSENUWlJWSGNyajQrYy83MGo1T3lJU1ZIK0IzUUltZgpVOWlNUzJvL3lCVXdDMm5zMVlxV1VQRGxoVEY3SnVpOXhEbWx4cXZubGUyVlROTytPMlBEK2p0WGdCakUya0Z3CiswanFvZCs1bXdMZUJ0Mno5bTEzYlVtUmZpVUFYWUg0YmlzOVhmVUNnWUVBbGJtYWhobXVaRjBTNzV6T0xrSnMKWHpwUlI1REkzaXdEN0lWYkNvK09uYXA1YW9PVHBhNjByRlV2NkhVMHZPK0o4VTgzRlNRS1lXMXNnTDZVazZMMApBek1PMno0N1U4Y1djdGlwS25GeGJkYmdhQTFvVDh2R2VPbk5MZ0Z2R1JpVEd1NG1LQUxxZStielp1dm9uM000CkQrZWJHYWtzRndFUDNkNkYzeXUrVHprQ2dZQTQ1RHE0V0IrQk4wNzFabFhDaGFGelo5Q001ejJrSkN3WHh1Ym4KRCt3Y3FZb2xZS09TVHI2a3UzaldEWnlOL1AxcjVBeDNZNGhIUEIyNUZzUExiTUQ5Z2U3dDdna0xPdFBFZEhMSgpuNEQwWXFLNEVyN3RKMjJPNXZyeWpDSmNyWGQ1V0ltVVlFUEVONDBVVjJuWVFEcmlQQXR0RTZwdjE0VGZKZmlhCnFFS1ZEUUtCZ0RuNktnSEU2NmY2VmR3UjFYN3R2WkVSOUo5d1pUWmJCZHpLZm81OWs4M0lRUXRFY0dxbnM2N0kKaXRuM1NZR3htUk5La2VzazNBQ2pKbTNEZyszTjNxakYrZjR1MTMyMmNTc3JCc2dQUEZIMGtwcGlyQS9tNmsvagowdmxBU1ZBa05WbHpZM3RONXJxRWtER2RFL21PNnE2ZlhMUkZkY201RGh2TzcwcHdhOUNhCi0tLS0tRU5EIFJTQSBQUklWQVRFIEtFWS0tLS0tCg==`)
+	jwtMgr.Init()
+	tokenByte, _ := jwtMgr.SignToken("test-client", "jwt provider", "jwt provider", time.Now().Add(time.Second*5))
+	auth := jwt.Authorizations{}
+	auth.Add("Bearer", string(tokenByte))
+	tokenStr := auth.String()
+	fmt.Println("token: ", tokenStr)
 
-    jwtProvider.Init()
+	parseTokenStr := jwt.ParseAuthorization(tokenStr).Get("Bearer")
+	fmt.Println("parse token:", parseTokenStr)
+	token, err := jwtMgr.ParseToken(parseTokenStr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(token.Audience(), token.Subject(), token.Issuer(), jwtMgr.TokenExpired(token))
 
-    ValidToken(&jwtProvider)
-    InvalidToken(&jwtProvider)
-    ExpiredToken(&jwtProvider)
+	time.Sleep(time.Second * 6)
 
-}
-
-func ValidToken(jwtProvider *jwt.JWKSProvider) {
-    mgr := jwt.NewJwtMgr(jwtProvider, true)
-    tokenByte, _ := mgr.SignToken("test-client", "jwt provider", "jwt provider", time.Now().Add(time.Hour*24), nil)
-    auth := jwt.Authorizations{}
-    auth.Add("Bearer", string(tokenByte))
-    token := auth.String()
-    fmt.Println("token: ", token)
-
-    // 校验token
-    t, err := mgr.ValidateTokenByTokenKey("Bearer", token)
-    if err != nil {
-        panic(err)
-    }
-    // 获取token Audience
-    fmt.Println(t.Audience())
-}
-
-func InvalidToken(jwtProvider *jwt.JWKSProvider) {
-    mgr := jwt.NewJwtMgr(jwtProvider, true)
-    tokenByte, _ := mgr.SignToken("test-client", "jwt provider", "jwt provider", time.Now().Add(time.Hour*24), nil)
-    auth := jwt.Authorizations{}
-    auth.Add("Bearer", string(tokenByte))
-
-    token := auth.String()
-
-    fmt.Println("token: ", token)
-
-    // 校验token
-    _, err := mgr.ValidateTokenByTokenKey("Bearer", token[2:])
-    if err != nil {
-        fmt.Println("error: ", err)
-    }
-
-}
-
-func ExpiredToken(jwtProvider *jwt.JWKSProvider) {
-    mgr := jwt.NewJwtMgr(jwtProvider, true)
-    tokenByte, _ := mgr.SignToken("test-client", "jwt provider", "jwt provider", time.Now().Add(time.Second), nil)
-    auth := jwt.Authorizations{}
-    auth.Add("Bearer", string(tokenByte))
-
-    token := auth.String()
-
-    fmt.Println("token: ", token)
-
-    time.Sleep(time.Second * 3)
-
-    // 校验token
-    _, err := mgr.ValidateTokenByTokenKey("Bearer", token)
-    if err != nil {
-        fmt.Println("error: ", err)
-    }
+	fmt.Println("expired ", token.Audience(), token.Subject(), token.Issuer(), jwtMgr.TokenExpired(token))
 }
--- jwt.go
+++ jwt.go
@@ -1,75 +1,98 @@
 package jwt
 
 import (
-    "fmt"
-    "github.com/google/uuid"
-    "github.com/lestrrat-go/jwx/jwa"
-    "github.com/lestrrat-go/jwx/jwt"
-    "time"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"github.com/google/uuid"
+	"github.com/lestrrat-go/jwx/v2/jwa"
+	"github.com/lestrrat-go/jwx/v2/jwk"
+	"github.com/lestrrat-go/jwx/v2/jwt"
+	"time"
 )
 
+var (
+	ErrKeyMustBePEMEncoded = errors.New("not pem encoded")
+	ErrNotRSAPrivateKey    = errors.New("not rsa private key")
+)
+
+func NewJwtMgr(privateKey string) *JwtMgr {
+	return &JwtMgr{
+		privateKey: privateKey,
+	}
+}
+
 type JwtMgr struct {
-    Provider *JWKSProvider
-
-    // 是否校验过期
-    checkExpired bool
+	privateKey    string
+	rsaPrivateKey jwk.Key
+	rsaPublicKey  jwk.Key
 }
 
-func NewJwtMgr(provider *JWKSProvider, checkExpired bool) *JwtMgr {
-    return &JwtMgr{
-        Provider:     provider,
-        checkExpired: checkExpired,
-    }
+func (c *JwtMgr) Init() {
+	pk, err := base64.StdEncoding.DecodeString(c.privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	r, err := c.parseRSAPrivateKeyFromPEM(pk)
+	if err != nil {
+		panic(err)
+	}
+
+	c.rsaPrivateKey, err = jwk.FromRaw(r)
+	if err != nil {
+		panic(err)
+	}
+
+	c.rsaPublicKey, err = jwk.PublicKeyOf(c.rsaPrivateKey)
+	if err != nil {
+		panic(err)
+	}
 }
 
-func (c *JwtMgr) Validate(tokenStr string) (jwt.Token, error) {
-    tok, err := c.validate(tokenStr)
-    if err != nil {
-        return nil, err
-    }
-    return tok, nil
+func (c *JwtMgr) parseRSAPrivateKeyFromPEM(key []byte) (*rsa.PrivateKey, error) {
+	var err error
+
+	// Parse PEM block
+	var block *pem.Block
+	if block, _ = pem.Decode(key); block == nil {
+		return nil, ErrKeyMustBePEMEncoded
+	}
+
+	var parsedKey interface{}
+	if parsedKey, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
+		if parsedKey, err = x509.ParsePKCS8PrivateKey(block.Bytes); err != nil {
+			return nil, err
+		}
+	}
+
+	var pkey *rsa.PrivateKey
+	var ok bool
+	if pkey, ok = parsedKey.(*rsa.PrivateKey); !ok {
+		return nil, ErrNotRSAPrivateKey
+	}
+
+	return pkey, nil
 }
 
-func (c *JwtMgr) validate(tokenStr string) (jwt.Token, error) {
-    tok, err := jwt.ParseString(tokenStr, jwt.WithKeySet(c.Provider.KeySet()))
-    if err != nil {
-        return nil, fmt.Errorf("valid Token Error:%s ", err.Error())
-    }
-    return tok, nil
+func (g *JwtMgr) SignToken(audienceKey, subjectKey, issuerKey string, expire time.Time) ([]byte, error) {
+	now := time.Now()
+	t := jwt.New()
+	_ = t.Set(jwt.AudienceKey, audienceKey)
+	_ = t.Set(jwt.SubjectKey, subjectKey)
+	_ = t.Set(jwt.IssuerKey, issuerKey)
+	_ = t.Set(jwt.JwtIDKey, uuid.New().String())
+	_ = t.Set(jwt.IssuedAtKey, now)
+	_ = t.Set(jwt.ExpirationKey, expire)
+	return jwt.Sign(t, jwt.WithKey(jwa.RS256, g.rsaPrivateKey))
 }
 
-func (c *JwtMgr) SignToken(audienceKey, subjectKey, issuerKey string, expire time.Time, opts map[string]interface{}) ([]byte, error) {
-    now := time.Now()
-    t := jwt.New()
-    _ = t.Set(jwt.AudienceKey, audienceKey)
-    _ = t.Set(jwt.SubjectKey, subjectKey)
-    _ = t.Set(jwt.IssuerKey, issuerKey)
-    _ = t.Set(jwt.JwtIDKey, uuid.New().String())
-    _ = t.Set(jwt.IssuedAtKey, now)
-    _ = t.Set(jwt.ExpirationKey, expire)
-
-    if opts != nil {
-        for k, v := range opts {
-            _ = t.Set(k, v)
-        }
-    }
-
-    return jwt.Sign(t, jwa.RS256, c.Provider.JWKForSign())
+func (c *JwtMgr) ParseToken(token string) (jwt.Token, error) {
+	return jwt.ParseString(token, jwt.WithKey(jwa.RS256, c.rsaPublicKey))
 }
 
-func (c *JwtMgr) ValidateTokenByTokenKey(tokenKey, token string) (jwt.Token, error) {
-
-    auth := ParseAuthorization(token)
-    t, err := c.Validate(auth.Get(tokenKey))
-    if err != nil {
-        return nil, err
-    }
-
-    if c.checkExpired {
-        if time.Now().Unix() > t.Expiration().Unix() {
-            return nil, fmt.Errorf("token expired")
-        }
-    }
-
-    return t, nil
+func (c *JwtMgr) TokenExpired(t jwt.Token) bool {
+	return time.Now().Unix() > t.Expiration().Unix()
 }
